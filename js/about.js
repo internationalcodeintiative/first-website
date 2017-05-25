afdea6489f0b20b0717bee253ac4d176988786f7
Brian Yang <brian.yang328@gmail.com>
2017-05-25 01:11:29 -0400

    Finish about page version 2

--- js/about.js
+++ js/about.js
@@ -1,15 +1,35 @@
 $(function() {
-	$('a[href*=#]').on('click', function(e) {
-		e.preventDefault();
+    $('a[href*=#]').on('click', function(e) {
+	e.preventDefault();
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-	});
+    });
 });
 
-$(document).ready(function(){
-    $('#scroller').slick({
-	infinite: true,
-	speed: 300,
-	slidesToShow: 1,
-	arrows: false,
-    });
+var fadeIn = false;
+
+$(document).ready(function() {
+    if (!fadeIn) {
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+            /* Check the location of each desired element */
+            $('.image').each( function(i){
+		// $(this).fadeIn(1500).css("display", "inline-block");
+		$(this).fadeIn(1000);
+	    });
+
+            $('.subtitle').each( function(i){
+		// $(this).fadeIn(1500).css("display", "inline-block");
+		$(this).fadeIn(3000);
+	    });
+
+	    $('.subsection').each( function(i){
+		// $(this).fadeIn(1500).css("display", "inline-block");
+		$(this).fadeIn(4000);
+	    });
+
+
+	});
+	fadeIn = true;
+    }
 });
